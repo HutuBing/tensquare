41b0db8cb00d7621c77499e559f2944bb2bb78b1
liaojb <1161086097@qq.com>
2019-08-13 14:19:03 +0800

    first commit

---
+++ tensquare_parent/tensquare_common/src/main/java/entity/PageResult.java
@@ -0,0 +1,21 @@
+package entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PageResult<T> {
+
+    private Long total;
+    private List<T> rows;
+
+    public PageResult() {
+    }
+
+    public PageResult(Long total, List<T> rows) {
+        this.total = total;
+        this.rows = rows;
+    }
+
+}
---
+++ tensquare_parent/tensquare_common/src/main/java/entity/Result.java
@@ -0,0 +1,30 @@
+package entity;
+
+import lombok.Data;
+
+@Data
+public class Result {
+
+    private boolean flag;
+    private Integer code;
+    private String msg;
+    private Object data;
+
+    public Result() {
+    }
+
+    public Result(boolean flag, Integer code, String msg, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Result(Object data) {
+        this.data = data;
+        this.flag = true;
+        this.msg = "success";
+        this.code = 2000;
+    }
+
+}
